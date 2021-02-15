769cdb8b78743da00153a1d6085331242a163d4a
Sander <SaWIEn99@outlook.com>
2021-02-15 19:01:17 +0100

    script
    Scriptet er faktisk ferdig, har muligheten til endring hvis vi har mer en en stat

---
+++ Assets/Score_Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score_Tracker : MonoBehaviour
+{
+    public int score, strikes;
+
+    public void changeScore(int x)
+    {
+        score += x;
+    }
+    public void takeStrike()
+    {
+
+    }
+    void Update()
+    {
+        if (score >=3)
+        {
+            gameOver();
+        }
+    }
+    public void gameOver()
+    {
+
+    }
+}
