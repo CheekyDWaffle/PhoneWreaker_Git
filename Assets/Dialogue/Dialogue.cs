68d1e4e168aca7cbc78ee8aa2c0f1f8923774a01
Martin <martin.knutsen@live.no>
2021-02-17 11:43:00 +0100

    Dialogue Scriptable Object
    Create -> Dialogue -> New Dialogue

---
+++ Assets/Dialogue/Dialogue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+    public class Dialogue : ScriptableObject {
+        public int npcID;
+        public string npcName;
+        public Message[] messages;
+        
+        
+    }
+[CreateAssetMenu(fileName = "New Dialogue", menuName ="Dialogue/New Dialogue")]
+public class DialogueData : Dialogue { }
+    
+
---
+++ Assets/Dialogue/Message.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+    public class Message {
+        public string text;
+        public Response[] responses;
+    }
