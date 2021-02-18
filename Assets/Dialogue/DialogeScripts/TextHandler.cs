4ceb5c28a41043af09d0044d5ec9078cac9c7b3f
Martin <martin.knutsen@live.no>
2021-02-18 18:12:30 +0100

    Started work on day 2

---
+++ Assets/Dialogue/DialogeScripts/TextHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+    
+
+public class TextHandler : MonoBehaviour
+{
+    public ScriptableDialogue dialogueObject;
+
+    public void LoadDialogue()
+    {
+        StartCoroutine(LoadText());
+    }
+    
+    IEnumerator LoadText()
+    {
+        for (int i = 0; i < dialogueObject.messages.Length; i++)
+        {
+            Debug.Log(dialogueObject.messages[i]);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
+
